c59e53ddb1b12fd8661624a9c3609176198c2e1a
atharvrawal <67418779+atharvrawal@users.noreply.github.com>
2024-04-20 09:19:40 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,48 +1,33 @@
-# prompt: get latitude and longititude coordinates from two different csv files and check the ones in 15km range of each other
-# where to upload file from
-
 import pandas as pd
-import pandas as pd
-from haversine import haversine, Unit
-# Read the first CSV file
-df1 = pd.read_csv('bbmp_lakes_masterlist-final.csv')
+import matplotlib
+from sklearn.linear_model import Ridge
+from sklearn.metrics import mean_absolute_error
+weather = pd.read_csv("2023-2024.csv",index_col = "datetime")
+weather.drop(columns = ["windgust","visibility","winddir","solarenergy","sunrise","sunset","windspeed","sealevelpressure","solarradiation","cloudcover","uvindex","severerisk","description","conditions","icon"],axis = 1,inplace = True)
+weather.index = pd.to_datetime(weather.index,format="mixed")
+weather["target"] = weather.shift(-1)["precip"]
+weather = weather.iloc[:-1,:].copy()
+reg = Ridge(alpha = .1)
+predictors = ["tempmax","tempmin","dew","humidity","moonphase"]
+weather = weather.sort_index()
+train = weather.loc[:'31-05-2023']
+test = weather.loc['01-06-2023':] 
+reg.fit(train[predictors],train["target"])
+predictions = reg.predict(test[predictors])
+combined = pd.concat([test["target"],pd.Series(predictions, index = test.index)],axis=1)
 
-# Read the second CSV file
-df2 = pd.read_csv('flooding_vulnerable_locations.csv')
+def create_predictions(predictions,weather, reg):
+    train = weather.loc[:'31-05-2023']
+    test = weather.loc['01-06-2023':] 
+    reg.fit(train[predictors],train["target"])
+    predictions = reg.predict(test[predictors])
+    error = mean_absolute_error(test["target"],predictions)
+    combined = pd.concat([test["target"],pd.Series(predictions, index = test.index)],axis=1)
+    combined.columns = ["actual","predictions"]
+    return error, combined
 
-# Extract latitude and longitude columns from both DataFrames
-lat1 = df1['latitude']
-lon1 = df1['longitude']
 
-lat2 = df2['latitude']
-lon2 = df2['longitude']
-
-# Calculate the distance between each pair of coordinates
-distances = []
-for i in range(len(lat1)):
-  for j in range(len(lat2)):
-    distance = haversine(df1.iloc[i][['latitude', 'longitude']], df2.iloc[j][['latitude', 'longitude']], unit=Unit.KILOMETERS)
-    distances.append(distance)
-
-# Find the pairs of coordinates that are within 15km of each other
-close_pairs = []
-for i, distance in enumerate(distances):
-  if distance <= 15:
-    close_pairs.append((df1.iloc[i], df2.iloc[j]))
-
-# Print the close pairs of coordinates
-for pair in close_pairs:
-  print(f"({pair[0]['latitude']}, {pair[0]['longitude']}), ({pair[1]['latitude']}, {pair[1]['longitude']})")
-
-# Function to calculate the distance between two coordinates
-def haversine_distance(lat1, lon1, lat2, lon2):
-  R = 6371  # Earth's radius in kilometers
-  dlat = degrees_to_radians(lat2 - lat1)
-  dlon = degrees_to_radians(lon2 - lon1)
-  a = sin(dlat / 2) ** 2 + cos(degrees_to_radians(lat1)) * cos(degrees_to_radians(lat2)) * sin(dlon / 2) ** 2
-  c = 2 * asin(sqrt(a))
-  return R * c
-
+print(create_predictions(predictions,weather,reg))
 # Function to convert degrees to radians
 def degrees_to_radians(degrees):
   return degrees * (pi / 180)
